dac88fc368e0f515d93d6dcc0325fe035f70788f
dpf205 <dpf205@gmail.com>
2017-09-17 20:36:07 -0400

    config

--- setup.py
+++ setup.py
@@ -5,18 +5,17 @@ import oandapy
 
 
 config = configparser.ConfigParser()
-
 config.read('./config/config.ini')
+
 account_id = config['oanda']['account_id']
 api_key = config['oanda']['api_key']
 
 # Get prices
-
 oanda = oandapy.API(environment="practice", access_token=api_key)
 
-fx_prices = oanda.get_prices(instruments="EUR_USD,USD_JPY,GBP_USD")
+response = oanda.get_prices(instruments="EUR_USD,USD_JPY,GBP_USD")
 
-data = fx_prices['prices']
+data = response['prices']
 time_stamp = data[0]['time']
 instrument = data[0]['instrument']
 bid_price = data[0]['bid']
@@ -25,9 +24,11 @@ ask_price = data[0]['ask']
 print("[{}] {} bid={} ask={}".format(time_stamp, instrument, bid_price, ask_price))
 
 
-table = pd.DataFrame(fx_prices['prices'])
+table = pd.DataFrame(response['prices'])
 pprint.pprint(table)
 
-# Obtain tadable instrument list
-instruments = oanda.get_instruments(account_id)
-print(instruments)
+
+response = oanda.get_instruments(account_id)
+pd.DataFrame(response['instruments']).head()
+
+
